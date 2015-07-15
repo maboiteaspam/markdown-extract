51530ca12bc7207962dde1bc95756a3e188233b6
clement <maboiteaspam@wanadoo.fr>
2015-07-15 23:25:49 +0800

    fix the tests !

--- test/index.js
+++ test/index.js
@@ -13,17 +13,17 @@ describe('markdown extract', function(){
   it('should find text given their type as a string', function(){
     mdExtract({type: 'heading'})[0].should.eql('markdown-extract')
     mdExtract({type: 'heading'})[1].should.eql('Installation')
-    mdExtract({type: 'heading'}).length.should.eql(6)
+    mdExtract({type: 'heading'}).length.should.eql(7)
   })
   it('should find text given their type as a regexp', function(){
     mdExtract({type: /heading/})[0].should.eql('markdown-extract')
     mdExtract({type: /heading/})[1].should.eql('Installation')
-    mdExtract({type: /heading/}).length.should.eql(6)
+    mdExtract({type: /heading/}).length.should.eql(7)
   })
   it('should find text given their type as an array', function(){
     mdExtract({type: ['heading', 'paragraph']})[0].should.eql('markdown-extract')
     mdExtract({type: ['heading', 'paragraph']})[2].should.eql('Installation')
-    mdExtract({type: ['heading', 'paragraph']}).length.should.eql(13)
+    mdExtract({type: ['heading', 'paragraph']}).length.should.eql(14)
   })
   it('should find next paragraph', function(){
     mdExtract({gnp: true, type: ['heading'], text: /Installation/}).length.should.eql(2)
@@ -31,7 +31,7 @@ describe('markdown extract', function(){
     mdExtract({gnp: true, type: 'heading', text: /Installation/})[1].should.eql('$ npm i markdown-extract --save')
   })
   it('should accept marked options as a second argument', function(){
-    mdExtract('heading', {gfm:false}).length.should.eql(9)
+    mdExtract('heading', {gfm:false}).length.should.eql(12)
     mdExtract('heading', {gfm:false})[0].should.eql('markdown-extract')
     mdExtract('heading', {gfm:false})[1].should.eql('Installation')
   })
@@ -40,7 +40,7 @@ describe('markdown extract', function(){
     mdExtract('heading', '# head1\ntext\n# head2\ntext\n')[0].should.eql('head1')
   })
   it('should accept MD content as third argument', function(){
-    mdExtract('heading', '# head1\ntext\n# head2\ntext\n').length.should.eql(2)
-    mdExtract('heading', '# head1\ntext\n# head2\ntext\n')[0].should.eql('head1')
+    mdExtract('heading', {gfm:false}, '# head1\ntext\n# head2\ntext\n').length.should.eql(2)
+    mdExtract('heading', {gfm:false}, '# head1\ntext\n# head2\ntext\n')[0].should.eql('head1')
   })
 })
